decd1f99adbf4e380f55d1f461b99d01f399840b
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-02-14 18:59:23 +0100

    added contains, and cumulative properbility attribute

--- TTStatistics/Dice.py
+++ TTStatistics/Dice.py
@@ -1,3 +1,4 @@
+from typing import Generator
 from itertools import product
 from TTStatistics._parser import faces_to_prop
 import TTStatistics.Pool as Pool 
@@ -10,28 +11,39 @@ class Dice(object):
 		self._f, self._p, self._c = faces_to_prop(faces)		
 		self._derived_attr()
 		self._mask = mask if mask else None
-
 	def _derived_attr(self):
 		self._mean = sum([p*f for p, f in zip(self.c, self.p)])
+		self._cdf = self._cumulative()
 
 	@property
 	def f(self) -> list:
 		return self._f
-
 	@property
 	def p(self) -> list:
 		return self._p
-	
+
+	@property
+	def c(self) -> list:
+		return self._c
+
+	@property
+	def cdf(self) -> list:
+		return self._cdf
+
 	def copy(self) -> Dice:
-		res = Dice([])
-		res._c = self._c
-		res._p = self._p
-		res._f = self._f
+		copy = Dice([])
+		copy._c = self.c
+		copy._p = self.p # should be getters and setters, so it easily change stuff
+		copy._f = self.f
+		return copy
 	
-	def __iter__(self):
+	def __iter__(self) -> Generator[int | float]: # might need ot be text also when mask
 		for f, c in zip(self.f, self.c):
 			for _ in range(c):
 				yield f
+				
+	def __contains__(self, value) -> bool:
+		return value in self._f
 
 	def __add__(self, rhs) -> Dice | Pool:
 		if isinstance(rhs, Dice):
@@ -41,3 +53,11 @@ class Dice(object):
 		else:
 			raise Exception("TODO add error dice")
 		
+	def _cumulative(self):
+		res = []
+		for p in self.p:
+			if res:
+				res.append(p + res[-1])
+			else:
+				res.append(p)
+		return res
