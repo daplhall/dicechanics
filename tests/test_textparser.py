ba24d6cdf627681addb76a5c407a4ecfdf69087c
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-03-03 11:16:31 +0100

    Dice can now take a list of faces with dice in them and thus expand them!

--- tests/test_textparser.py
+++ tests/test_textparser.py
@@ -1,45 +1,46 @@
 import unittest
 import dice_unittest
 
-from DiceStatistics._parser import text_to_faces, faces_to_count
+from DiceStatistics._parser import text_to_faces
+from DiceStatistics._inpt_cleaning import collect_faces
 
 class TestParser(dice_unittest.TestCase):
 	def test_parse(self):
 		f = text_to_faces("1,2,3,4,5,6")
-		f, c= faces_to_count(f)
-		self.assertSequenceEqual(f, [1,2,3,4,5,6])
-		self.assertSequenceEqual(c, [1,1,1,1,1,1])
+		f = collect_faces(f)
+		self.assertSequenceEqual(list(f.keys()), [1,2,3,4,5,6])
+		self.assertSequenceEqual(list(f.values()), [1,1,1,1,1,1])
 
 	def test_parse_float(self):
 		f = text_to_faces("1.5,2,3.2,4,5,6")
-		f, c= faces_to_count(f)
-		self.assertSequenceEqual(f, [1.5,2.0,3.2,4.0,5.0,6.0])
-		self.assertSequenceEqual(c, [1,1,1,1,1,1])
+		f= collect_faces(f)
+		self.assertSequenceEqual(list(f.keys()), [1.5,2.0,3.2,4.0,5.0,6.0])
+		self.assertSequenceEqual(list(f.values()), [1,1,1,1,1,1])
 		
 
 	def test_parse_count(self):
 		f = text_to_faces("1,1,1,2,3,4")
-		f, c= faces_to_count(f)
-		self.assertSequenceEqual(f, [1,2,3,4])
-		self.assertSequenceEqual(c, [3,1,1,1])
+		f = collect_faces(f)
+		self.assertSequenceEqual(list(f.keys()), [1,2,3,4])
+		self.assertSequenceEqual(list(f.values()), [3,1,1,1])
 
 	def test_parse_repeat(self):
 		f = text_to_faces("1:5,6")
-		f, c= faces_to_count(f)
-		self.assertSequenceEqual(f, [1,6])
-		self.assertSequenceEqual(c, [5,1])
+		f= collect_faces(f)
+		self.assertSequenceEqual(list(f.keys()), [1,6])
+		self.assertSequenceEqual(list(f.values()), [5,1])
 
 	def test_parse_range(self):
 		f = text_to_faces("1..5,6")
-		f, c= faces_to_count(f)
-		self.assertSequenceEqual(f, [1,2,3,4,5,6])
-		self.assertSequenceEqual(c, [1,1,1,1,1,1])
+		f= collect_faces(f)
+		self.assertSequenceEqual(list(f.keys()), [1,2,3,4,5,6])
+		self.assertSequenceEqual(list(f.values()), [1,1,1,1,1,1])
 
 	def test_parse_range_repeat(self):
 		f = text_to_faces("1..5:4,6")
-		f, c = faces_to_count(f)
-		self.assertSequenceEqual(f, [1,2,3,4,5,6])
-		self.assertSequenceEqual(c, [4,4,4,4,4,1])
+		f = collect_faces(f)
+		self.assertSequenceEqual(list(f.keys()), [1,2,3,4,5,6])
+		self.assertSequenceEqual(list(f.values()), [4,4,4,4,4,1])
 
 if __name__ == '__main__':
 	unittest.main()
