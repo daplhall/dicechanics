026cd5f059fad96496c4dd30eda3304727366d4c
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-05-07 11:06:23 +0200

    an alternative to outcomes2 is made, might be easier to cache

--- outcomes3.py
+++ outcomes3.py
@@ -1,3 +1,4 @@
+
 #from line_profiler import profile
 from itertools import compress
 from collections import ChainMap, defaultdict
@@ -10,7 +11,7 @@ def _max(key):
 	if not key:
 		return None
 	return max(key)
-		
+
 #@profile
 HITS = 0
 CALLS = 0
@@ -18,34 +19,28 @@ CALLS = 0
 the keep idea works we, we just need to know how to structure the order of the code corretly!
 and then we need to find out if we can cache
 """
-def comb(bag, func, keep, mem):
-	cache_key = (tuple(i) for i in bag)
-	if cache_key in mem:
-		return mem[cache_key]
-		
-	if not bag:
-		return {None:1}
+def dp(bag, func, keep, mem):
+	res = defaultdict(int)
+	C = 1 ## where this is used, we need to translate into dict
+	if len(bag) == 1:
+		for sf in bag[0]:
+			res[(sf,sf)] = C
+		return res
 
 	sorted_bag = sorted(bag, key = lambda key: _max(key), reverse=True) # might need copy
-	res = defaultdict(int)
 	while(sorted_bag[0]):
-		o = sorted_bag[0][-1] if keep[-1] else None
-		c = 1
-		sorted_bag[0] = sorted_bag[0][:-1]# "cheeper pop the top"
+		o = sorted_bag[0][-1]
+		c = C
+		sorted_bag[0] = sorted_bag[0][:-1]
 		sub_bag = sorted_bag[1:]
-		sub = comb(sub_bag, func, keep[:-1], mem)
-		for sf, sc in sub.items():
-			if o is None and sf is not None:
-				key = sf
-			elif sf is None:
-				key = o
-			else:
-				key= func(o,sf)
-			res[key] += c*sc
+		sub = dp(sub_bag, func, keep, mem)
+		for (sf, p), sc in sub.items():
+			if p <= o:
+				res[func(o,sf), o] += c*sc
 		if sorted_bag and len(sorted_bag) > 1 and sorted_bag[0]:
 			sorted_bag.sort(key = lambda key: _max(key), reverse=True)
-	mem[cache_key] = res
 	return res
+		
 
 def f(x,y):
 	if isinstance(y, int):
@@ -56,14 +51,17 @@ bag = [
 	[1,2],
 	[1,2],
 	[1,2],
-	[1,2]
+	[1,3],
 ]
 
-keep = [1,1,1,1]
+keep = [0,1]
 
 mem = {}
-res = comb(bag, lambda x,y: x+y, keep, mem)
+res = dp(bag, lambda x,y: x+y, keep, mem)
+final = defaultdict(int)
+for (f,_), c in res.items():
+	final[f] += c
+print(res)
+print(final)
+print(Dice.from_dict(final).p)
 #res = comb(bag, f)
-q = Dice.from_dict(res)
-print(q)
-print(q.p)
