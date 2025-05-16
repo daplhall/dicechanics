bb395e7ad72e347734538209134c7e44594584fe
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-05-16 20:07:38 +0200

    changed max to min and it runs faster, keeping doesn't seem to work that well

--- outcomes2.py
+++ outcomes2.py
@@ -1,4 +1,4 @@
-#from line_profiler import profile
+from line_profiler import profile
 from itertools import compress
 from collections import ChainMap, defaultdict
 from math import prod, factorial
@@ -9,31 +9,52 @@ from DiceStatistics import d, Dice
 def _max(key):
 	if not key:
 		return None
-	return max(key)
+	return min(key)
 		
-#@profile
 HITS = 0
 CALLS = 0
+RE = 0
+CACHEKEY = defaultdict(int)
 """
 the keep idea works we, we just need to know how to structure the order of the code corretly!
 and then we need to find out if we can cache
 """
-def comb(bag, func, keep, mem):
-	cache_key = (tuple(i) for i in bag)
+#:@profile
+def comb(bag, idxs, func, keep, mem):
+	global HITS, CALLS, RE
+	CALLS+=1
+
+	cache_key = tuple(tuple(i) for i in bag)
+	#print(bag, idxs)
 	if cache_key in mem:
+		HITS +=1
+		#print("\thit",bag, idxs,"\t\t",cache_key)
+		#CACHEKEY[cache_key] += 1
 		return mem[cache_key]
 		
+		
 	if not bag:
-		return {None:1}
+		res = {None: 1}
+		mem[cache_key] = res
+		return res
 
-	sorted_bag = sorted(bag, key = lambda key: _max(key), reverse=True) # might need copy
 	res = defaultdict(int)
+	sorted_bag, sorted_idxs = zip(*sorted(zip(bag,idxs), key = lambda key: _max(key[0]), reverse=True))
+	sorted_bag = list(sorted_bag)
 	while(sorted_bag[0]):
+		RE += 1
 		o = sorted_bag[0][-1] if keep[-1] else None
 		c = 1
 		sorted_bag[0] = sorted_bag[0][:-1]# "cheeper pop the top"
 		sub_bag = sorted_bag[1:]
-		sub = comb(sub_bag, func, keep[:-1], mem)
+		sub_idxs = sorted_idxs[1:]
+		#if len(bag) == 4:
+		print(o,sorted_idxs[0], sorted_bag[0], "\t", sub_bag, sub_idxs)
+		ck = tuple(tuple(i) for i in sub_bag)
+		if ck in mem:
+			sub = mem[ck]
+		else:
+			sub = comb(sub_bag, sub_idxs, func, keep[:-1], mem)
 		for sf, sc in sub.items():
 			if o is None and sf is not None:
 				key = sf
@@ -43,7 +64,8 @@ def comb(bag, func, keep, mem):
 				key= func(o,sf)
 			res[key] += c*sc
 		if sorted_bag and len(sorted_bag) > 1 and sorted_bag[0]:
-			sorted_bag.sort(key = lambda key: _max(key), reverse=True)
+			sorted_bag, sorted_idxs = zip(*sorted(zip(sorted_bag,sorted_idxs), key = lambda key: _max(key[0]), reverse=True))
+			sorted_bag = list(sorted_bag)
 	mem[cache_key] = res
 	return res
 
@@ -53,17 +75,38 @@ def f(x,y):
 	return (x,) + y
 
 bag = [
-	[1,2],
-	[1,2],
-	[1,2],
-	[1,2]
+		[1,2,3,4],
+		[2,3,4,5],
+		[3,4,5,6],
+		[4,5,6,7]
 ]
 
-keep = [1,1,1,1]
+ER = 10
+#bag = [
+#	list(range(1,ER)),
+#	list(range(1,ER)),
+#	list(range(1,ER)),
+#]
+bag = [
+	list(range(1,7)),
+	list(range(1,5)),
+]
 
+keep = [0,1]
+import time
 mem = {}
-res = comb(bag, lambda x,y: x+y, keep, mem)
-#res = comb(bag, f)
+t = time.time()
+#res = comb(bag, list(range(len(bag))), lambda x,y: x+y, keep, mem)
+res = comb(bag, list(range(len(bag))), lambda x,y: x+y, keep, mem)
+print(time.time() - t)
 q = Dice.from_dict(res)
+print(q._units)
+w = d(bag[0]) + d(bag[1])
 print(q)
-print(q.p)
+print(w)
+print(HITS)
+print(CALLS)
+print(HITS/CALLS)
+print(RE)
+for f, c in CACHEKEY.items():
+	print(f,c)
