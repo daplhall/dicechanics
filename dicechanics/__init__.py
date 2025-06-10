339bca28d8dd493ec32d52d0eae5c1cca9441307
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-06-10 13:00:09 +0200

    refatered the module interface

--- dicechanics/__init__.py
+++ dicechanics/__init__.py
@@ -1,6 +1,5 @@
 from dicechanics._Dice import Dice
 from dicechanics._Pool import Pool
-from dicechanics._BooleanDice import BooleanDice
 from dicechanics._interface import d, z, pool
 from dicechanics._standard import d4,d6,d8,d10,d12,d20,d100,z9
 
