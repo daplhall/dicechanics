c77c25cd8e6c6ae843c2a642666b445f0f329611
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-06-10 13:01:19 +0200

    refactored __init__.py

--- dicechanics/__init__.py
+++ dicechanics/__init__.py
@@ -1,5 +1,5 @@
 from dicechanics._Dice import Dice
 from dicechanics._Pool import Pool
 from dicechanics._interface import d, z, pool
-from dicechanics._standard import d4,d6,d8,d10,d12,d20,d100,z9
+from dicechanics._standard import d4, d6, d8, d10, d12, d20, d100, z9
 
