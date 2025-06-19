f5a633f48ddd6a7b2998691dfc6062fbe7980a6d
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-06-19 10:13:01 +0200

    added docstrings to _math

--- dicechanics/_math.py
+++ dicechanics/_math.py
@@ -2,6 +2,21 @@ from typing import Iterable
 
 
 def unique(array: Iterable[float]) -> tuple[list, list]:
+	"""
+	Function that finds the unique values in an iterable and their occurences.
+
+	Paramters
+	---------
+	array: Iterable
+		The array of objects which one wants to find the unique values of.
+
+	Returns
+	-------
+	uniques: list
+		The unique values.
+	count: list
+		The occurences of the unique values.
+	"""
 	uf = []
 	c = []
 	for e in array:
@@ -16,7 +31,17 @@ def unique(array: Iterable[float]) -> tuple[list, list]:
 
 def gcd(a: int, b: int) -> int:
 	"""
-	Greatest commen demoninator
+	Find the greatest commen demoninator between a and b
+
+	Parameters
+	----------
+	a: int
+	b: int
+
+	Returns
+	-------
+	out: int
+		Greatest common demominator
 	"""
 	while b != 0:
 		r = a % b
