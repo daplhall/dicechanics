72386824c9189b9bc3c6f0bcb81123082896fd86
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-06-25 17:40:57 +0200

    added _interface as it was renamed also added a ruff comment for ops docstring

--- dicechanics/ops.py
+++ dicechanics/ops.py
@@ -1,6 +1,6 @@
 """
 Module that contains some simple linear operations to use with :py:class:`dicechanics.Pool`
-"""
+"""  # noqa: E501
 
 __all__ = ["add", "sub", "mul", "floordiv", "div", "mod", "ceil", "floor"]
 
