edf983d3406f0e0dc2c3796613f46919c73a7044
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-06-25 11:10:21 +0200

    added docstrings to interface

--- dicechanics/interface.py
+++ dicechanics/interface.py
@@ -5,23 +5,67 @@ from dicechanics.Die import Die
 from dicechanics.Pool import Pool
 
 
-def d(inpt: Iterable | int, **kwards) -> Die:
+def d(inpt: Iterable | int | Die | dict, **kwards) -> Die:
+	"""
+	Function that creates a die from various inpt types
+	Currently takes int, str, Iterable, Die which it copies, and a dict
+
+	Parameters
+	----------
+	inpt: Iterable | Int | Die | dict
+		the input for the function
+
+	Returns
+	-------
+	out: Die
+		The die created from the input
+	"""
 	if isinstance(inpt, int):
 		return Die(range(1, inpt + 1), **kwards)
 	elif isinstance(inpt, str):
 		faces = text_to_faces(inpt)
 		return Die(faces, **kwards)
+	elif isinstance(inpt, Die):
+		return inpt.copy()
+	elif isinstance(inpt, dict):
+		return Die.from_dict(inpt)
 	elif isinstance(inpt, Iterable):
 		return Die(inpt, **kwards)
-	elif isinstance(inpt, Die):
-		return inpt
 	else:
 		raise Exception(f"Dice doens't support input type of {type(inpt)}")
 
 
 def z(inpt: int, **kwards) -> Die:
+	"""
+	Function that creates a z-die from various inpt types
+
+	A z-die is a die which starts from 0. So a z9 is a die going from [0..9]
+
+	Parameters
+	----------
+	inpt: int
+		the input for the function
+
+	Returns
+	-------
+	out: Die
+		The die created from the input
+	"""
 	return Die(range(0, inpt + 1), **kwards)
 
 
 def pool(inpt: list) -> Pool:
+	"""
+	Creates a pool from a list of number and/or dice.
+
+	Parameters
+	----------
+	inpt: list
+		list of numbers or dice
+
+	Returns
+	-------
+	out: Pool
+		The pool containing the elements of the input
+	"""
 	return Pool(inpt)
