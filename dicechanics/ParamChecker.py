1d72a4e7e9daf26a0a480918c44debb1cf50b4a5
Daniel Ploug Hall <46996860+daplhall@users.noreply.github.com>
2025-08-08 10:48:34 +0200

    changed recomendation to levenshtein distance, and refactored the code

--- dicechanics/ParamChecker.py
+++ dicechanics/ParamChecker.py
@@ -1,27 +1,65 @@
 import inspect
 from typing import Tuple
 
+"""
+the idea is that when pool is used as a decorator we do a check on the function
+
+if you want to do it with a perform, its a good idea to use @pool_func decorator
+which checks it for you when the function is defined
+"""
+
+
+class UnsupportedParamters(Exception):
+	def __init__(self, matches, wrong_types, function):
+		msg = ""
+		if wrong_types:
+			for param, curr_type, corr_type in wrong_types:
+				msg += f"* Wrong Type - Parameter {param}\n"
+				msg += f"\t it is '{curr_type.__name__}' it should be '{corr_type.__name__}'\n"
+		if matches:
+			for written, match in matches:
+				msg += (
+					f"* Parameter '{written}' is not supported, did you mean:\n"
+				)
+				msg += f"\t- {match}\n"
+		super().__init__(
+			f"\nError in the signature of '{function.__name__}' in {inspect.getsourcefile(function)}\n"
+			+ msg
+		)
+
+
+def lev(a: str, b: str):
+	"""
+	levenshtein_distance
+	https://en.wikipedia.org/wiki/Levenshtein_distance
+	"""
+	if len(b) == 0:
+		return len(a)
+	elif len(a) == 0:
+		return len(b)
+	elif a[0] == b[0]:
+		return lev(a[1:], b[1:])
+	else:
+		return 1 + min(lev(a[1:], b), lev(a, b[1:]), lev(a[1:], b[1:]))
+
 
 class OptionsMatcher:
 	"""
 	needs to contain options
 	Idea:
-	https://en.wikipedia.org/wiki/Levenshtein_distance
 	https://stackoverflow.com/questions/5859561/getting-the-closest-string-match
 	"""
 
 	def __init__(self, options: dict):
-		self.options = {i: set(i) for i in options.keys()}
+		self.options = list(options.keys())
 
-	def match(self, arg, acpt_crit=0.7) -> Tuple[bool, float]:
+	def match(self, arg) -> Tuple[bool, float]:
 		""" """
 		tmp = []
-		for option, matcher in self.options.items():
-			prcnt_match = len(matcher & set(arg)) / len(matcher)
-			if prcnt_match >= acpt_crit:
-				tmp.append((option, prcnt_match))
-		tmp.sort(key=lambda key: key[1], reverse=True)
-		return [i for i, _ in tmp]
+		for option in self.options:
+			tmp.append((lev(option, arg), option))
+		tmp.sort()
+		return tmp[0][1]
 
 
 class Signature:
@@ -48,7 +86,7 @@ class ParamChecker(Signature):
 		"""
 		TODO I am not checking fully illegals
 		"""
-		wrong_type = []
+		wrong_types = []
 		params = ParamChecker.signature(function)
 		missing = set(params) - set(self.options)
 		hits = set(params) & set(self.options)
@@ -56,27 +94,13 @@ class ParamChecker(Signature):
 			options = filter(lambda x: x[0] in hits, params.items())
 			for param, mytype in options:
 				if mytype != self.options[param]:
-					wrong_type.append((param, mytype, self.options[param]))
+					wrong_types.append((param, mytype, self.options[param]))
 		my_matches = []
 		for miss in missing:
 			if matches := self.matcher.match(miss):
 				my_matches.append((miss, matches))
-		# TODO make Exception raising its own special exception
-		msg = ""
-		if wrong_type:
-			for param, curr_type, corr_type in wrong_type:
-				msg += f"Wrong Type - Parameter {param}\n"
-				msg += f"\t it is '{curr_type.__name__}' it should be '{corr_type.__name__}'\n"
-		if my_matches:
-			for written, matches in my_matches:
-				msg += f"Parameter {written} is not supported, did you mean:\n"
-				for match in matches:
-					msg += f"\t- {match}\n"
-		if msg:
-			raise Exception(
-				f"\nError in the signature of '{function.__name__}' in {inspect.getsoucefile(function)}\n"
-				+ msg
-			)
+		if my_matches or wrong_types:
+			raise UnsupportedParamters(my_matches, wrong_types, function)
 		return True
 
 	@classmethod
@@ -107,11 +131,11 @@ print(test_template_empty.options)
 print("================")
 
 
-def sanity(x, y, maimum, minim):
+def sanity(x, y, maimum, minim, frog):
 	pass
 
 
-test_template.check(sanity)
+test_template_types.check(sanity)
 
 
 if __name__ == "__main__":
